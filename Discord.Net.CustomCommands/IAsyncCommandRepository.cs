e08f167dab4efbcd8f5b36008b2ab6e848bf8247
Brian Ebeling <bje@live.de>
2018-07-05 21:40:12 +0200

    Update CommandService to act like a Repository 2

---
+++ Discord.Net.CustomCommands/IAsyncCommandRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Discord.Net.CustomCommands
+{
+    public interface IAsyncCommandRepository<TContext>
+    {
+        Task<CommandBase<TContext>> Get(string userMessage, CancellationToken cancellationToken);
+    }
+}

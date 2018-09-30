19d81b2640df62d7e955c623655af52f94b18cc7
Sid-003 <siddhartha4030@gmail.com>
2018-09-30 18:39:15 -0400

    Added IsUser condition
    - is pretty useless

---
+++ Rethought.Commands.Discord.Net/Conditions/HasRole.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using Rethought.Commands.Conditions;
+
+namespace Rethought.Commands.Discord.Net.Conditions
+{
+    public class IsUser : ICondition<DiscordContext>
+    {
+        public ulong[] UserIds { get; }
+
+        public IsUser(params ulong[] userIds)
+            => UserIds = userIds;
+
+        public bool Satisfied(DiscordContext context)
+        {
+            if (UserIds.Any(x => x.Id == context.User.Id))
+                return true;
+            return false;
+        }
+    }
+}

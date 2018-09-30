8edb40f7257bdfb7d190e4fe6d6299b6bcee103f
Sid-003 <siddhartha4030@gmail.com>
2018-09-30 17:38:17 -0400

    Added IsSafeChannel
    also might not work

---
+++ Rethought.Commands.Discord.Net/Conditions/IsSafeChannel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rethought.Commands.Discord.Net.Conditions
+{
+    public class IsSafeChannel : ICondition<DiscordContext>
+    {
+        public bool Satisfied(DiscordContext context)
+            => context.Channel is SocketTextChannel socketTextChannel && !socketTextChannel.IsNsfw;
+    }
+}
